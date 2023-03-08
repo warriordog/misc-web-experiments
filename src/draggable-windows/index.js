6b43ca5d5d170aa024e9b21b95d9481ed349506d
Hazel Koehler <acomputerdog@gmail.com>
2023-03-07 20:59:01 -0500

    Implement vertical window manager

--- src/draggable-windows/index.js
+++ src/draggable-windows/index.js
@@ -1,16 +1,23 @@
 import './HWindowElement.js';
 
-const button = document.querySelector('#newWindowButton')
-if (button) {
-    button.addEventListener('click', () => {
-        const win = document.createElement('h-window');
-        win.windowTitle = 'New Window';
-        win.body.innerHTML = '<p>You just made a new window!<br>Try moving it around and interacting with other windows.</p>';
+/** @type {HTMLButtonElement | null} */
+const newWindowButton = document.querySelector('#newWindowButton');
+newWindowButton?.addEventListener('click', () => {
+    const win = document.createElement('h-window');
+    win.windowTitle = 'New Window';
+    win.body.innerHTML = '<p>You just made a new window!<br>Try moving it around and interacting with other windows.</p>';
 
-        document.body.append(win);
-        win.activate();
-    });
-}
+    document.body.append(win);
+    win.activate();
+});
+
+/** @type {HTMLSelectElement | null} */
+const wmSelect = document.querySelector('#wmSelector');
+wmSelect?.addEventListener('change', () => {
+   const wm = document.querySelector('#wm');
+   wm?.classList.toggle('vertical-wm', wmSelect.value === 'vertical');
+   wm?.classList.toggle('none-wm', wmSelect.value === 'none');
+});
 
 // Pre-arrange the windows
 const windows = document.querySelectorAll('h-window');
